fe5209fbd4796b45a8a932b01bbadb2f2ef43415
ghost <kbinas2022@elam.co.uk>
2023-06-19 15:35:21 +0100

    player rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
-    public float rotationSpeed = 3; 
+    public float rotationSpeed; 
     public float rotation = 0.0f; 
 
     Rigidbody rigidBody;
+/* 
+    public Transform self;
+    public float turn_speed = 1f;
+    */ 
  
     void Awake()
     {
@@ -18,11 +22,37 @@ public class PlayerMovement : MonoBehaviour
  
     void FixedUpdate()
     {
-        float inputH = Input.GetAxis("Horizontal");
-        float inputV = Input.GetAxis("Vertical");
-        rotation = rotation + Input.GetAxis("Vertical") * rotationSpeed;
-        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
-        rigidBody.velocity = new Vector3(inputH * speed, rigidBody.velocity.y, inputV * speed);
+        float horizontalInput = Input.GetAxis("Horizontal"); 
+        float verticalInput=Input.GetAxis("Vertical"); 
+
+        //rotation = rotation - Input.GetAxis("Vertical") * rotationSpeed;
+        //transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+        /*
+        Vector3 targetDirection = new Vector3(self.position.x + inputH, self.position.y, self.position.z + inputV);
+        float singleStep = speed * Time.deltaTime;
+
+        Vector3 newDirection = Vector3.RotateTowards(transform.forward, targetDirection, singleStep, 0.0f);
+
+        self.rotation = Quaternion.LookRotation(newDirection);
+        */ 
+
+        /* 
+        player_Direction = transform.rotation * new Vector3(player_Horizontal, 0.0f, player_Vertical);
+        */ 
+
+        // Calculates the direction the player is moving 
+        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput); 
+        movementDirection.Normalize(); 
+        transform.Translate(movementDirection * speed * Time.deltaTime, Space.World); 
+
+        if (movementDirection != Vector3.zero) 
+        {
+        // Direction of where the player looks the character will rotation 
+            Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up); 
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime); 
+        }
+
         
     }
 }
